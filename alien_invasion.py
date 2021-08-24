7c6270cb8f45bdf8812090becb5603889737c336
phillipcs <pgibsoncs@gmail.com>
2021-08-23 19:42:15 -0500

    first commit

---
+++ alien_invasion.py
@@ -0,0 +1,70 @@
+import sys
+
+from settings import Settings
+from ship import Ship
+
+import pygame
+
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior"""
+
+    def __init__(self):
+        """Initialize the game, and create game resources"""
+        pygame.init()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
+
+        # Set the background color.
+        self.bg_color = (0, 0, 255)
+
+    def run_game(self):
+        """Start the main loop for the game"""
+        while True:
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
+
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+    def _check_keydown_events(self, event):
+        """Respond to keypresses."""
+        if event.key == pygame.K_RIGHT:
+            # Move the ship to the right
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+            #fjkdljaklfd
+
+
+if __name__ == '__main__':
+    # Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
