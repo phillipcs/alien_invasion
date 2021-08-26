5d0782f5b902b0a14eb50eae9572dcfb1b66ea34
phillipcs <pgibsoncs@gmail.com>
2021-08-25 19:13:07 -0500

    Added Alien class along with functionality to put aliens on screen and have them move down the screen toward the player.

--- settings.py
+++ settings.py
@@ -9,7 +9,7 @@ class Settings:
 		self.bg_color = (230, 230, 230)
 
 		# Ship settings
-		self.ship_speed = 1.5
+		self.ship_speed = 1.0
 
 		# Bullet Settings
 		self.bullet_speed = 1.0
@@ -17,5 +17,12 @@ class Settings:
 		self.bullet_height = 15
 		self.bullet_color = (60, 60, 60)
 		self.bullets_allowed = 3
+
+		# alien settings
+		self.alien_speed = 0.2
+		self.fleet_drop_speed = 10
+		# fleet_direction of 1 represents right; -1 represents left.
+		self.fleet_direction = 1
+
 
 
