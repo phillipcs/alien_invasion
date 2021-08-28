089dc4bd89814acd768e751703bc83a7d5786639
phillipcs <pgibsoncs@gmail.com>
2021-08-28 09:31:00 -0500

    Added lives for player added collision events when bullet hits alien, alien hits player, and alien hits bottom of screen added reset when alien hits player or alien hits bottom of screen added condition for game over when players loses all three lives added game_stats to keep track of player lives

--- settings.py
+++ settings.py
@@ -10,17 +10,18 @@ class Settings:
 
 		# Ship settings
 		self.ship_speed = 1.0
+		self.ship_limit = 3
 
 		# Bullet Settings
-		self.bullet_speed = 1.0
-		self.bullet_width = 3
+		self.bullet_speed = 0.5
+		self.bullet_width = 4
 		self.bullet_height = 15
 		self.bullet_color = (60, 60, 60)
 		self.bullets_allowed = 3
 
 		# alien settings
 		self.alien_speed = 0.2
-		self.fleet_drop_speed = 10
+		self.fleet_drop_speed = 100
 		# fleet_direction of 1 represents right; -1 represents left.
 		self.fleet_direction = 1
 
